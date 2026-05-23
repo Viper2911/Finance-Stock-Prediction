09b35b61a34aa54beb01620c4c40cde891fbebbe
Aadil Hasan <aadilhasan29.5@gmail.com>
2026-05-23 13:48:24 +0530

    Initial app.py pushed

--- app.py
+++ app.py
@@ -5,3 +5,17 @@ import pandas as pd
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
+
+st.set_page_config(page_title="Market Forecast",layout="wide")
+
+START="2015-01-01"
+TODAY=date.today().strftime("%Y-%m-%d")
+
+st.title('Upgraded Market Forecast App')
+st.write("Powered by Prophet Machine Learning + Technical Indicators")
+
+stocks = ('AAPL', 'MSFT', 'GOOGL', 'TSLA', 'NVDA', 'RELIANCE.NS', 'TCS.NS')
+selected_stock = st.selectbox('Select dataset for prediction', stocks)
+
+n_days=st.slider('Days of prediction into the future:',1,30,7)
+
